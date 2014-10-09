c1ca42f246c16e6d7e3da396340cec48ac60dca1
aclel <andrew.cleland3@gmail.com>
2014-10-10 09:44:24 +1000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MarketLanding::Application.config.secret_key_base = 'b5259872debb5fa1b2bba878ce264fc85d90984b14d415e829811b5e0e14680a85b69b554c3430f2701821980ced09606772de6cf042d11e90fbb5ebc137688b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MarketLanding::Application.config.session_store :cookie_store, key: '_market-landing_session'
